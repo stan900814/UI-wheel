15ef14907022f49ee41c64c54e906a381139b8c1
stan900814 <362328861@qq.com>
2020-02-29 11:52:57 +0800

    将svg抽离成单文件组件

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-      <svg v-if="icon" class="icon">
-        <use :xlink:href="`#i-${icon}`" />
-      </svg>
+    <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+      <g-icon v-if="icon" :name='icon'></g-icon>
       <div class="content">
         <slot></slot>
       </div>
@@ -14,55 +11,32 @@
 <script>
 export default {
   name: "",
-   props: {
-      icon: {},
-      iconPosition: {
-        type: String,
-        default: 'left',
-        validator (value) {
-          return value === 'left' || value === 'right'
-        }
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
       }
     }
+  }
 };
 </script>
 <style lang="scss">
-.g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  vertical-align: middle;
-  &:hover {
-    border-color: var(--border-color-hover);
-  }
-  &:active {
-    background-color: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
-  }
-  > .content {
-    order: 2;
-  }
-  > .icon {
-    order: 1;
-    margin-right: 0.1em;
-  }
-  &.icon-right {
-    > .content {
-      order: 1;
-    }
-    > .icon {
-      order: 2;
-      margin-right: 0;
-      margin-left: 0.1em;
+.g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
+    border-radius: var(--border-radius); border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex; justify-content: center; align-items: center;
+    vertical-align: middle;
+    &:hover { border-color: var(--border-color-hover); }
+    &:active { background-color: var(--button-active-bg); }
+    &:focus { outline: none; }
+    > .content { order: 2; }
+    > .icon { order: 1; margin-right: .1em; }
+    &.icon-right {
+      > .content { order: 1; }
+      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
     }
   }
-}
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <svg class="g-icon">
+      <use :xlink:href="`#i-${name}`" />
+    </svg>
+  </div>
+</template>
+<script>
+export default {
+  props: ['name']
+};
+</script>
+<style lang="scss">
+.g-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
