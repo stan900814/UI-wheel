08ddd8f0461787b95fdfaefe0f48dcfe2980736e
stan900814 <362328861@qq.com>
2020-03-02 14:52:53 +0800

    添加4个单元测试

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import Buttongroup from './button-group'
+import chai from 'chai'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -15,3 +16,66 @@ new Vue({
         loading1:false
     }
 })
+
+//单元测试
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon:'settings'
+        }
+    })
+    vm.$mount()
+    let use = vm.$el.querySelector('use')
+    expect(use.getAttribute('xlink:href')).to.eq('#i-settings')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading:true
+        }
+    })
+    vm.$mount() //button.$mount()
+    let use = vm.$el.querySelector('use')
+    expect(use.getAttribute('xlink:href')).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
