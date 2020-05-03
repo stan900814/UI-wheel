baab374acba1870b861058b543a015309e4a1990
stan900814 <362328861@qq.com>
2020-05-03 18:22:06 +0800

    优化row和col的代码

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[span&&`col-${span}`,offset&&`offset-${offset}`]" :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+    <div class="col" :class="colClass" :style="colStyle">
         <slot></slot>
     </div>
 </template>
@@ -18,6 +18,16 @@
         data(){
             return{
                 gutter:0,
+            }
+        },
+        computed:{
+            colStyle(){
+                let {gutter} = this
+                return {paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}
+            },
+            colClass(){
+                let {span,offset} = this
+                return [span&&`col-${span}`,offset&&`offset-${offset}`]
             }
         }
     }
