9652cb6ecc1564f5547f6086985244bbb4b01486
stan900814 <362328861@qq.com>
2020-05-04 16:09:13 +0800

    格式化

--- src/row.vue
+++ src/row.vue
@@ -1,54 +1,54 @@
 <template>
-    <div class="row" :style="rowStyle" :class="rowClass">
-        <slot></slot>
-    </div>
+  <div class="row" :style="rowStyle" :class="rowClass">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "GuluRow",
-        props:{
-            gutter:{
-                type:[Number,String]
-            },
-            align:{
-                type: String,
-                validator(value){
-                    return ['left','center','right'].includes(value)
-                }
-            }
-        },
-        computed:{
-            rowStyle(){
-                let {gutter} = this
-                return {marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}
-            },
-            rowClass(){
-                let {align} = this
-                return[align&&`align-${align}`]
-            }
-        },
-        mounted() {
-            this.$children.forEach((vm)=>{
-                vm.gutter = this.gutter
-            })
+  export default {
+    name: "GuluRow",
+    props:{
+      gutter:{
+        type:[Number,String]
+      },
+      align:{
+        type: String,
+        validator(value){
+          return ['left','center','right'].includes(value)
         }
+      }
+    },
+    computed:{
+      rowStyle(){
+        let {gutter} = this
+        return {marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}
+      },
+      rowClass(){
+        let {align} = this
+        return[align&&`align-${align}`]
+      }
+    },
+    mounted() {
+      this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+      })
     }
+  }
 </script>
 
 <style scoped lang="scss">
-    .row{
-        display: flex;
-        flex-wrap: wrap;
-        &.align-left{
-            justify-content: flex-start;
-        }
-        &.align-center{
-            justify-content: center;
-        }
-        &.align-right{
-            justify-content: flex-end;
-        }
+  .row{
+    display: flex;
+    flex-wrap: wrap;
+    &.align-left{
+      justify-content: flex-start;
     }
+    &.align-center{
+      justify-content: center;
+    }
+    &.align-right{
+      justify-content: flex-end;
+    }
+  }
 
 </style>
