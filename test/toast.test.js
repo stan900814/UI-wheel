8f99515980120e668857adcc608ed974b15c9ea0
stan900814 <362328861@qq.com>
2020-05-05 19:10:20 +0800

    通过toast的测试用例

---
+++ test/toast.test.js
@@ -0,0 +1,61 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Toast from '../src/toast'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Toast', () => {
+
+  it('存在.', () => {
+    expect(Toast).to.exist
+  })
+
+  describe('props', function () {
+    this.timeout(15000)
+    it('接受 autoClose', (done) => {
+      let div = document.createElement('div')
+      document.body.appendChild(div)
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          autoClose: true,
+          autoCloseDelay: 3
+        }
+      }).$mount(div)
+      vm.$on('close', () => {
+       console.log('close')
+      })
+      setTimeout(()=>{
+        expect(document.body.contains(vm.$el)).to.eq(false)
+        done()
+      },5000)
+    })
+    it('接受 closeButton', () => {
+      const callback = sinon.fake();
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          closeButton: {
+            text: '关闭吧',
+            callback,
+          },
+        }
+      }).$mount()
+      let closeButton = vm.$el.querySelector('.close')
+      expect(closeButton.textContent.trim()).to.eq('关闭吧')
+      closeButton.click()
+      expect(callback).to.have.been.called
+    })
+
+    it('接受 position', () => {
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          position: 'bottom'
+        }
+      }).$mount()
+      expect(vm.$el.classList.contains('position-bottom')).to.eq(true)
+    })
+  })
+})
